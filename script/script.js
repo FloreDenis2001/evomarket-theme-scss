e21d0f0139576436481bd975b62cc68f302074c8
FloreDenis2001 <floredenis907@yahoo.com>
2023-10-15 02:06:12 +0300

    last_update

--- script/script.js
+++ script/script.js
@@ -1,33 +1,7 @@
 const progressCircle = document.querySelector(".autoplay-progress svg");
 const progressContent = document.querySelector(".autoplay-progress span");
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 3,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
 
-        400:{
-            slidesPerView: 3.3,
-            spaceBetween: 3,
-        },
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 50,
-      },
-    },
-});
 
 var swiper = new Swiper(".mySwiper-products", {
     slidesPerView: 2,
@@ -39,19 +13,22 @@ var swiper = new Swiper(".mySwiper-products", {
     breakpoints: {
         400:{
             slidesPerView: 2.5,
-            spaceBetween: 20,
+            spaceBetween: 3,
         },
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
+     600: {
+        slidesPerView:3.5,
+        spaceBetween:3,
+      },768: {
+        slidesPerView:4.5,
+        spaceBetween:3,
       },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 40,
+      900: {
+        slidesPerView: 4.5,
+        spaceBetween: 3,
       },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 50,
+      1200: {
+        slidesPerView: 4.5,
+        spaceBetween: 3,
       },
     },
 });
@@ -81,3 +58,30 @@ var swiper = new Swiper(".mySwiper-posters", {
 });
 
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 3,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+
+        400:{
+            slidesPerView: 3.3,
+            spaceBetween: 3,
+        },
+      640: {
+        slidesPerView: 3.5,
+        spaceBetween: 3,
+      },
+      768: {
+        slidesPerView: 4.5,
+        spaceBetween: 3,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween:3,
+      },
+    },
+});

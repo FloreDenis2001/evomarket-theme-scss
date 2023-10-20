9406ae946a6aa26d0e3c75218b2225e4c9767f9e
FloreDenis2001 <floredenis907@yahoo.com>
2023-10-20 12:36:31 +0300

    last_update

--- script/script.js
+++ script/script.js
@@ -11,7 +11,47 @@ var swiper = new Swiper(".mySwiper--posters", {
   },
 });
 
-var swiper = new Swiper(".mySwiper-products", {
-  watchSlidesProgress: true,
-  slidesPerView: 4,
+var swiper;
+
+function initSwiper() {
+  var screenWidth = window.innerWidth;
+
+  var slidesPerView = 4; // Numărul inițial de carduri pe slide
+
+  if (screenWidth <= 900) {
+    slidesPerView = 2; // Pentru tablete, afișează 2 carduri pe slide
+  }
+
+  if (screenWidth <= 600) {
+    slidesPerView = 1; // Pentru telefoane, afișează 1 card pe slide
+  }
+
+  // Inițializează sau actualizează Swiper cu noile opțiuni
+  if (swiper) {
+    swiper.destroy();
+  }
+
+  swiper = new Swiper(".mySwiper-products", {
+    slidesPerView: slidesPerView,
+    spaceBetween: 52, // Ajustează spațiul între carduri așa cum dorești
+    slidesPerGroup: slidesPerView,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+}
+
+// Inițializează Swiper la încărcarea paginii
+window.addEventListener("load", function () {
+  initSwiper();
+});
+
+// Actualizează Swiper când se redimensionează fereastra
+window.addEventListener("resize", function () {
+  initSwiper();
 });
